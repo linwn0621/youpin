3a35924947c74af73d26a4a419e462c2626a18e3
jackyfive <1833792912@qq.com>
2020-03-15 14:51:58 +0800

    商品详情页面轮播图到图文详情完成

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,3 +1,4 @@
+import { request } from "../../request/request.js"
 // pages/goods_detail/index.js
 Page({
 
@@ -5,7 +6,9 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    Goodsdetail: {},
+    // 设置默认收藏条件
+    isCollect: false
   },
 
   /**
@@ -13,8 +16,69 @@ Page({
    */
   onLoad: function (options) {
 
-  },
+    this.getGoodsdetail(options)
 
+  },
+  // 获取商品详情数据
+  async getGoodsdetail(e) {
+    const Goodsdetail = await request({
+      url: "/goods/detail",
+      data: e
+    })
+    // 判断本地储存中有没有收藏
+    const collect = wx.getStorageSync("collect") || []
+    const isCollect = collect.some(v => v.goods_id === Goodsdetail.goods_id)
+    this.setData({
+      Goodsdetail, isCollect
+    })
+    console.log(Goodsdetail)
+  },
+  // 点击图片放大
+  handleCilck(e) {
+    const {
+      pics,
+      img
+    } = e.currentTarget.dataset;
+    wx.previewImage({
+      current: img, // 当前显示图片的http链接
+      urls: pics.map(v => v.pics_big_url) // 需要预览的图片http链接列表
+    })
+    console.log(e.target)
+
+  },
+  // 点击收藏时
+  handleshoucang() {
+    // 获取本地存储
+    let collect = wx.getStorageSync("collect") || []
+    console.log(collect)
+    // 判断商品id是否在本地存储中
+    let index = collect.findIndex(v => v.goods_id === this.data.Goodsdetail.goods_id);
+    console.log(index)
+    if (index != -1) {
+      collect.splice(index, 1)
+      this.setData({
+        isCollect: false
+      })
+      wx.showToast({
+        title: '取消成功',
+        icon: "success",
+        mask: true
+      })
+      wx.setStorageSync("collect", collect)
+    } else {
+      collect.push(this.data.Goodsdetail)
+      this.setData({
+        isCollect: true
+      })
+      wx.showToast({
+        title: '收藏成功',
+        icon: "success",
+        mask: true
+      })
+      wx.setStorageSync("collect", collect)
+    }
+
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
