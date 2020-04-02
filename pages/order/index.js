1b4a22ec352b0173b61177029ca0b67de23014c3
jackyfive <1833792912@qq.com>
2020-04-02 10:27:59 +0800

    订单列表页完成

--- pages/order/index.js
+++ pages/order/index.js
@@ -1,20 +1,68 @@
-// pages/order/index.js
+
+import {
+  request
+} from "../../request/request.js"// pages/order/index.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    orders: [],
+    tabs: [{
+      id: 0,
+      value: "全部",
+      isActive: true,
+      type: 1,
+    }, {
+      id: 1,
+      value: "待付款",
+      isActive: false,
+      type: 2,
+    }, {
+      id: 2,
+      value: "待发货",
+      isActive: false,
+      type: 3, //订单类型
+    }],
+    type: 1
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    const type = 1
+    this.getorders(type)
   },
-
+  // 获取订单数据
+  async getorders(type) {
+    // 请求 拿到对应 type 的数据
+    let { orders } = await request({
+      url: "/my/orders/all",
+      data: {
+        type
+      }
+    })
+    console.log(orders)
+    orders.forEach(v => {
+      v["create_time_cn"] = (new Date(v.create_time * 1000).toLocaleString())
+    })
+    this.setData({
+      orders
+    })
+  },
+  // 点击切换
+  handletabsChange(e) {
+    console.log(e.detail)
+    let tabs = e.detail;
+    tabs.map(async v => {
+      if (v.isActive) {
+        let type = v.type
+        this.getorders(type)
+      }
+    });
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
