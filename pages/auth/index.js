02f47e85d08a7c446d5400b932dafb88261d51d6
jackyfive <1833792912@qq.com>
2020-04-02 10:21:57 +0800

    授权页面完成

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,4 +1,8 @@
 // pages/auth/index.js
+import {
+  request
+} from "../../request/request.js"
+import { login } from "../../utils/asyncWx.js"
 Page({
 
   /**
@@ -12,6 +16,38 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+
+  },
+  async handleUserInfo(e) {
+    console.log(e);
+    try {
+      //通过button 获取到的参数信息
+      // encryptedData, iv, rawData, signature
+      let {
+        encryptedData, iv, rawData, signature
+      } = e.detail;
+      //通过 wx.login 获取 code 参数
+      // https://developers.weixin.qq.com/miniprogram/dev/api/open-api/login/wx.login.html
+      let { code } = await login();
+      //console.log(code);
+      //发请求 获取token 数据 
+      const res = await request({
+        url: "/users/wxlogin",
+        method: "POST", //post
+        data: {
+          encryptedData, iv, rawData, signature, code
+        }
+      });
+
+      
+      wx.setStorageSync("token", res.token)
+      wx.navigateBack({
+        delta: -1
+      })
+      // console.log(code)
+    } catch (e) {
+      console.log(e)
+    }
 
   },
 
